dd9041a55fe7d7df8becda6e0f7bd1e841019454
Lazycoder229 <restyg931@gmail.com>
2025-08-22 11:29:10 +0800

    Fixed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,23 @@
-import blogRoutes from './blog.js';
+import blogRoutes from "./blog.js";
 
+import featureRoutes from "./feature.js";
 
-import featureRoutes from './feature.js';
-
-
-import { Router } from 'express';
-import { render } from '../lib/view.js';
-import path from 'path';
+import { Router } from "express";
+import { render } from "../lib/view.js";
+import path from "path";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  render(res, 'pages/home.html', { title: 'Home' });
+router.get("/", (req, res) => {
+  render(res, "pages/home.html", { title: "Home" });
 });
 
-router.get('/learn', (req, res) => {
-  render(res, 'pages/learn.html', { title: 'Learn' });
+router.get("/learn", (req, res) => {
+  render(res, "pages/learn.html", { title: "Learn" });
 });
 
 // Auto-load .html pages from views/pages/
-router.get('/:page.html', (req, res, next) => {
+router.get("/:page.html", (req, res, next) => {
   const page = req.params.page;
   try {
     render(res, `pages/${page}.html`, { title: page });
@@ -28,11 +26,8 @@ router.get('/:page.html', (req, res, next) => {
   }
 });
 
+router.use("/feature", featureRoutes);
 
-
-router.use('/feature', featureRoutes);
-
-
-router.use('/blog', blogRoutes);
+router.use("/blog", blogRoutes);
 
 export default router;
